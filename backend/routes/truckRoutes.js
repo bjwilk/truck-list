531a88977585635ea446363f44a01c68c7bd83a6
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-05-22 15:59:26 -0700

    added uploadRouter and user sign up

--- backend/routes/truckRoutes.js
+++ backend/routes/truckRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('./cors');
 
 const Truck = require("../models/truck");
 const truckRouter = express.Router();
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const truckRouter = require('./routes/truckRoutes')
 const connectDB = require('./mongoConnection')
+const uploadRouter = require('./routes/uploadRouter');
 
 const PORT = 3001
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/truck', truckRouter)
+app.use('/imageUpload', uploadRouter);
 
 app.get('/', (req, res) => {
 res.send('hello trucks')
