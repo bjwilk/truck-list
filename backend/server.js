6ae8dc893992739783dfa96bcdf026e07c0da88c
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-07-09 15:09:20 -0700

    build deploy

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,27 @@ const uploadRouter = require('./routes/uploadRouter');
 const userRoute = require('./routes/users')
 const PORT = 3001
 const app = express()
+const path = require('path')
+
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "../build")))
+
+
 app.use('/truck', truckRouter)
 app.use('/imageUpload', uploadRouter);
 app.use('/users', userRoute);
 
-app.get('/', (req, res) => {
-res.send('hello trucks')
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"))
 })
+
+// app.get('/', (req, res) => {
+// res.send('hello trucks')
+// })
 
 app.listen(process.env.PORT || PORT, async () => {
     await connectDB();
